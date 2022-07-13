33ee5392e78c1657346eacb7830d40690034c995
MD Jamil Kashem Porosh <50365984+mdjamilkashemporosh@users.noreply.github.com>
2022-07-13 11:57:43 +0600

    Update Header.tsx
    Responsive header 1.0.0

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -3,16 +3,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { CgProfile } from 'react-icons/cg';
+import { BiSearch } from 'react-icons/bi';
 import { MdOutlineAccountBalanceWallet } from 'react-icons/md';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import openseaLogo from '../../Assets/opensea.png';
 
 const style = {
-    wrapper: `flex py-4 px-12 justify-between items-center`,
-    logoContainer: `flex items-center`,
-    logoText: ` ml-[0.5rem] text-white font-semibold text-2xl`,
-    searchBar: `flex items-center bg-[#363840] rounded-[0.5rem] hover:bg-[#4c505c]`,
-    searchIcon: `text-[#8a939b] mx-3 text-2xl`,
-    searchInput: `h-[3rem] w-[50rem] text-lg border-0 bg-transparent outline-none pr-2 duration-300 text-[#e6e8eb] placeholder:text-[#8a939b]`,
+    wrapper: `flex py-4 md:px-4 lg:px-12 justify-between items-center`,
+    logoContainer: `flex items-center justify-center w-[200px]`,
+    logoText: ` mx-[0.5rem] text-white font-semibold text-2xl`,
+    searchBar: `flex items-center lg:bg-[#363840] rounded-[0.5rem] lg:hover:bg-[#4c505c]`,
+    searchIcon: `text-[#8a939b] mx-3 text-3xl lg:text-2xl`,
+    searchInput: `h-[3rem] w-0 lg:w-[50rem] text-lg border-0 bg-transparent outline-none pr-2 duration-300 text-[#e6e8eb] placeholder:text-[#8a939b]`,
     headerItems: ` flex items-center`,
     headerItem: `text-white text-lg px-4 font-semibold text-[#c8cacd] hover:text-white`,
     headerIcon: `text-[#8a939b] text-4xl px-6 hover:text-white`,
@@ -27,12 +29,14 @@ const Header = () => {
                     <div className={style.logoText}>Opensea</div>
                 </a>
             </Link>
+            <div className="flex justify-center items-center">
             <div className={style.searchBar}>
                 <div className={style.searchIcon}>
                     <AiOutlineSearch />
                 </div>
                 <input className={style.searchInput} placeholder="Search items, collections, and accounts" />
             </div>
+            <div className="hidden">
             <div className={style.headerItems}>
                 <Link href=""><a className={style.headerItem}> Collections </a></Link>
                 <Link href=""><a className={style.headerItem}> Stats </a></Link>
@@ -43,6 +47,12 @@ const Header = () => {
                 <Link href=""><a className={style.headerIcon}><CgProfile /></a></Link>
                 <Link href=""><a className={style.headerIcon}><MdOutlineAccountBalanceWallet /></a></Link>
             </div>
+            </div>
+                {/* <div className="flex"> */}
+                {/* <Link href=""><a className={style.headerIcon}><BiSearch /></a></Link> */}
+                <Link href=""><a className={style.headerIcon}><GiHamburgerMenu /></a></Link>
+                {/* </div> */}
+                </div>
         </div>
     )
 }
