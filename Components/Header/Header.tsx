6c9b25ee20d05d2307de0e2ac1180562302eb19d
mdjamilkashemporosh <jamilkashem2002@gmail.com>
2022-07-14 15:33:02 +0600

    Desktop header responsiveness update added

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { BiSearch } from 'react-icons/bi';
 import { CgProfile } from 'react-icons/cg';
-import {GiHamburgerMenu } from 'react-icons/gi';
-import {FaTimes } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { FaTimes } from 'react-icons/fa';
 import { MdOutlineAccountBalanceWallet } from 'react-icons/md';
 import openseaLogo from '../../Assets/opensea.png';
 
@@ -25,31 +25,33 @@ function Header() {
                     <p className='text-2xl mx-1 font-semibold'>Opensea</p>
                 </a>
             </Link>
-            <div className='flex flex-col items-center lg:flex-row text-white'>
-                {/* <div className='flex items-center'>
-                    <BiSearch />
-                    <input placeholder='Search items, collections, and accounts...' />
-                </div> */}
-                <div className={`flex  flex-col duration-300 lg:flex-row absolute lg:static inset-0 lg:inset-auto	bg-[#04111d] lg:bg-transparent bg-opacity-95 z-50 mt-24 lg:mt-0 font-semibold overflow-hidden ${openContentStyle}`}>
-                <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Explore </a></Link>
-                {/* <Link href=""><a className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Collections </a></Link> */}
-                <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Stats </a></Link>
-                <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Resources </a></Link>
-                <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Create </a></Link>
-                <div className='flex my-2 absolute lg:static bottom-0 lg:bottom-auto lg:ml-12 justify-center items-center w-screen lg:w-auto'>
-                <Link href=""><a onClick={isClicked} className='text-4xl p-6 text-white hover:text-opacity-50 duration-300'><CgProfile /></a></Link>
-                <Link href=""><a onClick={isClicked} className='text-4xl p-6 text-white hover:text-opacity-50 duration-300'><MdOutlineAccountBalanceWallet /></a></Link>
+            <div className='flex items-center justify-between text-white lg:w-[75vw]'>
+                <div className='flex items-center'>
+                    <div className='rounded-l-md lg:pl-6 pr-2 h-14 flex items-center text-3xl lg:bg-white lg:bg-opacity-10'>
+                        <BiSearch />
+                    </div>
+                    <input type='text' className='rounded-r-md text-lg outline-none w-0 lg:w-[40rem] bg-white bg-opacity-10 lg:px-4 h-14' placeholder='Search items,collections, and accounts...' />
                 </div>
+                <div className={`flex  flex-col duration-300 lg:flex-row absolute lg:static inset-0 lg:inset-auto	bg-[#04111d] lg:bg-transparent bg-opacity-95 z-50 mt-24 lg:mt-0 font-semibold overflow-hidden ${openContentStyle}`}>
+                    <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Explore </a></Link>
+                    {/* <Link href=""><a className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Collections </a></Link> */}
+                    <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Stats </a></Link>
+                    <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Resources </a></Link>
+                    <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Create </a></Link>
+                    <div className='flex my-2 absolute lg:static bottom-0 lg:bottom-auto lg:ml-12 justify-center items-center w-screen lg:w-auto'>
+                        <Link href=""><a onClick={isClicked} className='text-4xl p-6 text-white hover:text-opacity-50 duration-300'><CgProfile /></a></Link>
+                        <Link href=""><a onClick={isClicked} className='text-4xl p-6 text-white hover:text-opacity-50 duration-300'><MdOutlineAccountBalanceWallet /></a></Link>
+                    </div>
                 </div>
                 {/* <p>Icon Kinks</p> */}
                 {/* Search icon and cross icon here */}
                 {/* <div onClick={isClicked}> onClick={isClicked} */}
 
- 
-                <Link  href=""><a onClick={isClicked} className={`lg:hidden text-4xl p-6 text-white hover:text-opacity-50 duration-300 ${openStyle}`}><FaTimes /></a></Link>
-                <Link  href=""><a onClick={isClicked} className={`lg:hidden text-4xl p-6 text-white hover:text-opacity-50 duration-300 ${closeStyle}`}><GiHamburgerMenu /></a></Link>
-            
-            {/* </div> onClick={isClicked} */}
+
+                <Link href=""><a onClick={isClicked} className={`lg:hidden text-4xl p-6 text-white hover:text-opacity-50 duration-300 ${openStyle}`}><FaTimes /></a></Link>
+                <Link href=""><a onClick={isClicked} className={`lg:hidden text-4xl p-6 text-white hover:text-opacity-50 duration-300 ${closeStyle}`}><GiHamburgerMenu /></a></Link>
+
+                {/* </div> onClick={isClicked} */}
             </div>
         </div>
     )
