e74678b9888937975f9faf0fd5cdb497ab7a0fe2
mdjamilkashemporosh <jamilkashem2002@gmail.com>
2022-07-17 10:36:55 +0600

    Responsive header optimization process 1.0.1

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -37,18 +37,18 @@ function Header() {
             </Link>
             <div className='flex items-center text-white'>
             {/* absolute inset-0 lg:inset-auto h-24 lg:h-auto */}
-                <div className={`flex items-center xl:p-6 lg:static ${searchInputWraper}`}>
-                    <div onClick={isClicked2} className={`rounded-l-md lg:pl-6   px-6 lg:pr-2 h-14 flex items-center text-3xl lg:bg-white lg:bg-opacity-10 ${openStyle2}`}>
+                <div className={`flex items-center 2xl:p-6 2xl:static ${searchInputWraper}`}>
+                    <div onClick={isClicked2} className={`rounded-l-md  px-6 2xl:h-14 flex items-center text-3xl lg:bg-white lg:bg-opacity-10 ${openStyle2}`}>
                         <BiSearch />
                     </div>
-                    <div onClick={isClicked2} className={`rounded-l-md lg:pl-6   px-6 lg:pr-2 h-14 flex items-center text-3xl lg:bg-white lg:bg-opacity-10 ${closeStyle2}`}>
+                    <div onClick={isClicked2} className={`px-6  flex items-center text-3xl  ${closeStyle2}`}>
                         <BiLeftArrowAlt />
                     </div>
                     {/* w-[85vw] */}
-                    <input type='text' className={`rounded-r-md text-lg outline-none pl-2 lg:pl-0 lg:w-[32rem] bg-transparent lg:bg-white lg:bg-opacity-10 lg:px-4 h-14 ${searchInput}`} placeholder='Search items,collections, and accounts...' />
+                    <input type='text' className={`2xl:rounded-r-md text-lg outline-none pl-4 2xl:pl-0 2xl:w-[32rem] bg-transparent 2xl:bg-white 2xl:bg-opacity-10 2xl:px-4 h-14 ${searchInput}`} placeholder='Search items,collections, and accounts...' />
                 </div>
-                <div className={`flex lg:ml-4 flex-col duration-300 lg:flex-row absolute lg:static inset-0 lg:inset-auto	bg-[#04111d] lg:bg-transparent bg-opacity-95 z-50 mt-24 lg:mt-0 font-semibold overflow-hidden ${openContentStyle}`}>
-                    <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Explore </a></Link>
+                <div className={`flex 2xl:ml-4 items-start 2xl:items-center  flex-col duration-300 lg:flex-row absolute lg:static inset-0 2xl:inset-auto	bg-gray-800 2xl:bg-transparent z-50 mt-24 2xl:mt-0 font-semibold ${openContentStyle}`}>
+                    <Link href=""><a onClick={isClicked} className='my-2 2xl:my-0 hover:bg-white hover:bg-opacity-5 2xl:bg-transparent rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Explore </a></Link>
                     {/* <Link href=""><a className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Collections </a></Link> */}
                     <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Stats </a></Link>
                     <Link href=""><a onClick={isClicked} className=' my-2 hover:bg-white  hover:bg-opacity-5 lg:bg-transparent lg:hover:bg-opacity-0 rounded p-6 hover:mx-2 lg:hover:mx-0 lg:hover:opacity-50 duration-300 text-xl'> Resources </a></Link>
