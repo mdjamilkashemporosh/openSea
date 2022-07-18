ea67290f10373a221451e2af72edd96159962bba
mdjamilkashemporosh <jamilkashem2002@gmail.com>
2022-07-18 10:36:53 +0600

    Responsive header optimization process 1.1.3

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -10,35 +10,37 @@ import openseaLogo from '../../Assets/opensea.png';
 
 
 function Header() {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [isOpen2, setIsOpen2] = React.useState(false);
+    const [mobileMenu, setMobileMenuStatus] = React.useState(false);
+    const [mobileSearchInput, setMobileSearchInputStatus] = React.useState(false);
 
 
 
-    const isClicked = () => {
-        setIsOpen(!isOpen);
+    const changeMobileMenuStatus = () => {
+        if (window.innerWidth <= 1536) {
+        setMobileMenuStatus(!mobileMenu);
+        }
     }
 
-    const isClicked2 = () => {
-        if (window.innerWidth <= 1024) {
-            setIsOpen2(!isOpen2);
+    const changeMobileSearchStatus = () => {
+        if (window.innerWidth <= 1536) {
+            setMobileSearchInputStatus(!mobileSearchInput)
         }
     }
     
-    const openStyle = isOpen ? `` : `hidden`;
-    const openStyle2 = isOpen2 ? `hidden` : ``;
-    const openContentStyle = isOpen ? `-translate-x-0` : `-translate-x-full lg:-translate-x-0`;
-    const closeStyle = isOpen ? `hidden` : ``;
-    const closeStyle2 = isOpen2 ? `` : `hidden`;
-    const searchInputWraper = isOpen2 ? `absolute inset-0 lg:inset-auto h-24 lg:h-auto bg-gray-800` : ``;
-    const searchInput = isOpen2 ? `w-[85vw]` : `w-0`;
+    const openStyle = mobileMenu ? `` : `hidden`;
+    const openStyle2 = mobileSearchInput ? `hidden` : ``;
+    const openContentStyle = mobileMenu ? `-translate-x-0` : `-translate-x-full 2xl:-translate-x-0`;
+    const closeStyle = mobileMenu ? `hidden` : ``;
+    const closeStyle2 = mobileSearchInput ? `` : `hidden`;
+    const searchInputWrapper = mobileSearchInput ? `absolute inset-0 lg:inset-auto h-24 lg:h-auto bg-gray-800` : ``;
+    const searchInput = mobileSearchInput ? `w-[85vw]` : `w-0`;
 
     const style = {
         container: 'flex justify-between p-4 lg:p-0 h-24',
         logoWrapper: 'flex text-white items-center',
         logoText: 'text-2xl mx-1 font-semibold',
         searchAndLinks: 'flex items-center text-white',
-        searchInputWrapper: `flex items-center 2xl:p-6 2xl:static ${searchInputWraper}`,
+        searchInputWrapper: `flex items-center 2xl:p-6 2xl:static ${searchInputWrapper}`,
         searchIcon: `rounded-l-md  px-6 2xl:h-14 flex items-center text-3xl lg:bg-white lg:bg-opacity-10 ${openStyle2}`,
         timesIcon: `px-6  flex items-center text-3xl  ${closeStyle2}`,
         searchInput: `2xl:rounded-r-md text-lg outline-none pl-4 2xl:pl-0 2xl:w-[32rem] bg-transparent 2xl:bg-white 2xl:bg-opacity-10 2xl:px-4 h-14 ${searchInput}`,
@@ -58,26 +60,26 @@ function Header() {
             </Link>
             <div className={style.searchAndLinks}>
                 <div className={style.searchInputWrapper}>
-                    <div onClick={isClicked2} className={style.searchIcon}>
+                    <div onClick={changeMobileSearchStatus} className={style.searchIcon}>
                         <BiSearch />
                     </div>
-                    <div onClick={isClicked2} className={style.timesIcon}>
+                    <div onClick={changeMobileSearchStatus} className={style.timesIcon}>
                         <BiLeftArrowAlt />
                     </div>
                     <input type='text' className={style.searchInput} placeholder='Search items,collections, and accounts...' />
                 </div>
                 <div className={style.quickLinksWrapper}>
-                    <Link href=""><a onClick={isClicked} className={style.quickLinks}> Explore </a></Link>
-                    <Link href=""><a onClick={isClicked} className={style.quickLinks}> Stats </a></Link>
-                    <Link href=""><a onClick={isClicked} className={style.quickLinks}> Resources </a></Link>
-                    <Link href=""><a onClick={isClicked} className={style.quickLinks}> Create </a></Link>
+                    <Link href=""><a onClick={changeMobileMenuStatus} className={style.quickLinks}> Explore </a></Link>
+                    <Link href=""><a onClick={changeMobileMenuStatus} className={style.quickLinks}> Stats </a></Link>
+                    <Link href=""><a onClick={changeMobileMenuStatus} className={style.quickLinks}> Resources </a></Link>
+                    <Link href=""><a onClick={changeMobileMenuStatus} className={style.quickLinks}> Create </a></Link>
                     <div className={style.quickIconLinksWrapper}>
-                        <Link href=""><a onClick={isClicked} className={style.iconLinks}><CgProfile /></a></Link>
-                        <Link href=""><a onClick={isClicked} className={style.iconLinks}><MdOutlineAccountBalanceWallet /></a></Link>
+                        <Link href=""><a onClick={changeMobileMenuStatus} className={style.iconLinks}><CgProfile /></a></Link>
+                        <Link href=""><a onClick={changeMobileMenuStatus} className={style.iconLinks}><MdOutlineAccountBalanceWallet /></a></Link>
                     </div>
                 </div>
-                <Link href=""><a onClick={isClicked} className={`lg:hidden ${style.iconLinks} ${openStyle}`}><FaTimes /></a></Link>
-                <Link href=""><a onClick={isClicked} className={`lg:hidden ${style.iconLinks} ${closeStyle}`}><GiHamburgerMenu /></a></Link>
+                <Link href=""><a onClick={changeMobileMenuStatus} className={`lg:hidden ${style.iconLinks} ${openStyle}`}><FaTimes /></a></Link>
+                <Link href=""><a onClick={changeMobileMenuStatus} className={`lg:hidden ${style.iconLinks} ${closeStyle}`}><GiHamburgerMenu /></a></Link>
             </div>
         </div>
     )
